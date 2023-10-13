f5678a93efe1cfdea78c9a6f6d24b5a05fc415c7
ulfatyousuf <snow10515@gmail.com>
2023-10-13 06:30:06 -0700

    Work Done

---
+++ calculator app/app.js
@@ -0,0 +1,18 @@
+function getNumber(num){
+var result = document.getElementById("result");
+result.value += num;
+}
+function clearResult() {
+    var result = document.getElementById("result");
+    result.value = "";  
+}
+
+function deleteNumber(){
+    var result = document.getElementById("result");
+    result.value = result.value.slice(0,-1);
+}
+
+function equal(){
+    var result = document.getElementById("result");
+    result.value = eval(result.value);
+}
